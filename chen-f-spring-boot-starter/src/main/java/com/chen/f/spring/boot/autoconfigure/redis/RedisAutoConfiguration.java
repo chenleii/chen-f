89099658124bd67064cb2e4276477445582e6809
chenlei <1@1.com>
2019-07-11 13:04:42 +0800

    去除缓存配置

--- chen-f-spring-boot-starter/src/main/java/com/chen/f/spring/boot/autoconfigure/redis/RedisAutoConfiguration.java
+++ chen-f-spring-boot-starter/src/main/java/com/chen/f/spring/boot/autoconfigure/redis/RedisAutoConfiguration.java
@@ -1,19 +1,16 @@
 package com.chen.f.spring.boot.autoconfigure.redis;
 
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,33 +21,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 @ConditionalOnClass(RedisOperations.class)
-@AutoConfigureBefore({org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
+@EnableConfigurationProperties(RedisProperties.class)
+@AutoConfigureAfter({org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
         org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration.class})
-@EnableCaching
 public class RedisAutoConfiguration {
 
-    @Primary
-    @Bean("reactiveRedisTemplateString")
-    @ConditionalOnBean(ReactiveRedisConnectionFactory.class)
-    public ReactiveRedisTemplate<String, String> reactiveRedisTemplateString(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
-        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, RedisSerializationContext.string());
-    }
 
     @Primary
-    @Bean("reactiveRedisTemplate")
-    @ConditionalOnBean(ReactiveRedisConnectionFactory.class)
-    public ReactiveRedisTemplate<?, ?> reactiveRedisTemplate(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
-        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, RedisSerializationContext.newSerializationContext()
-                .hashKey(new GenericJackson2JsonRedisSerializer())
-                .hashValue(new GenericJackson2JsonRedisSerializer())
-                .key(new GenericJackson2JsonRedisSerializer())
-                .value(new GenericJackson2JsonRedisSerializer())
-                .string(new StringRedisSerializer())
-                .build());
-    }
-
-    @Primary
-    @Bean("redisTemplate")
+    @Bean("redisTemplate1")
     @ConditionalOnBean(RedisConnectionFactory.class)
     public RedisTemplate<String, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
@@ -63,7 +41,7 @@ public class RedisAutoConfiguration {
     }
 
     @Primary
-    @Bean("stringRedisTemplate")
+    @Bean("stringRedisTemplate1")
     @ConditionalOnBean(RedisConnectionFactory.class)
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
@@ -73,13 +51,22 @@ public class RedisAutoConfiguration {
     }
 
     @Primary
-    @Bean
-    @ConditionalOnBean(RedisConnectionFactory.class)
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheManager redisCacheManager = RedisCacheManager.create(connectionFactory);
-        redisCacheManager.setTransactionAware(true);
-        return redisCacheManager;
+    @Bean("reactiveRedisTemplate1")
+    @ConditionalOnBean(ReactiveRedisConnectionFactory.class)
+    public ReactiveRedisTemplate<?, ?> reactiveRedisTemplate(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
+        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, RedisSerializationContext.newSerializationContext()
+                .hashKey(new GenericJackson2JsonRedisSerializer())
+                .hashValue(new GenericJackson2JsonRedisSerializer())
+                .key(new GenericJackson2JsonRedisSerializer())
+                .value(new GenericJackson2JsonRedisSerializer())
+                .string(new StringRedisSerializer())
+                .build());
     }
 
-
+    @Primary
+    @Bean("reactiveStringRedisTemplate1")
+    @ConditionalOnBean(ReactiveRedisConnectionFactory.class)
+    public ReactiveStringRedisTemplate reactiveRedisTemplateString(ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
+        return new ReactiveStringRedisTemplate(reactiveRedisConnectionFactory, RedisSerializationContext.string());
+    }
 }
