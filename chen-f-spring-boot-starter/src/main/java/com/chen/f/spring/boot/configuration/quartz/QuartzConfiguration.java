5fc7b64c0e72bc8a7f713b6956650fdec99b63d5
chenlei <1@1.com>
2019-07-25 16:24:57 +0800

    quartz配置

--- chen-f-spring-boot-starter/src/main/java/com/chen/f/spring/boot/configuration/quartz/QuartzConfiguration.java
+++ chen-f-spring-boot-starter/src/main/java/com/chen/f/spring/boot/configuration/quartz/QuartzConfiguration.java
@@ -1,12 +1,12 @@
 package com.chen.f.spring.boot.configuration.quartz;
 
+import com.chen.f.admin.quartz.TaskLogJobListener;
 import com.chen.f.core.mapper.SysTimedTaskLogMapper;
 import com.chen.f.core.mapper.SysTimedTaskMapper;
-import com.chen.f.admin.quartz.TaskLogJobListener;
 import com.chen.f.spring.boot.autoconfigure.mybatisplus.MybatisPlusAutoConfiguration;
-import org.quartz.JobListener;
+import com.chen.f.spring.boot.exception.ChenFConfigurationException;
 import org.quartz.Scheduler;
-import org.springframework.beans.factory.ObjectProvider;
+import org.quartz.SchedulerException;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -17,9 +17,12 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
 /**
+ * quartz配置
+ * <p>
+ * 每次执行完任务重新储存任务数据 {@link org.quartz.PersistJobDataAfterExecution}
+ * 禁止并发执行 {@link org.quartz.DisallowConcurrentExecution}
+ *
  * @author chen
- * @PersistJobDataAfterExecution
- * @DisallowConcurrentExecution
  * @date 2018/10/27 23:09.
  */
 @Configuration
@@ -28,21 +31,19 @@ import org.springframework.transaction.PlatformTransactionManager;
 @AutoConfigureAfter(MybatisPlusAutoConfiguration.class)
 public class QuartzConfiguration {
 
-    @ConditionalOnClass({SysTimedTaskLogMapper.class, SysTimedTaskMapper.class})
-    @Bean
-    public JobListener taskLogJobListener(SysTimedTaskLogMapper sysTimedTaskLogMapper, SysTimedTaskMapper sysTimedTaskMapper) {
-        return new TaskLogJobListener(sysTimedTaskLogMapper, sysTimedTaskMapper);
-    }
 
     /**
      * 在 Customizer 中处理自定义SchedulerFactoryBean配置
      */
+    @ConditionalOnClass({SysTimedTaskLogMapper.class, SysTimedTaskMapper.class})
     @Bean
-    public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer(ObjectProvider<JobListener[]> jobListeners) {
+    public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer(SysTimedTaskLogMapper sysTimedTaskLogMapper, SysTimedTaskMapper sysTimedTaskMapper) {
         return (schedulerFactoryBean) -> {
-            schedulerFactoryBean.setGlobalJobListeners(jobListeners.getIfAvailable());
-
-
+            try {
+                schedulerFactoryBean.getScheduler().getListenerManager().addJobListener(new TaskLogJobListener(sysTimedTaskLogMapper, sysTimedTaskMapper));
+            } catch (SchedulerException e) {
+                throw new ChenFConfigurationException("quartz scheduler job listener configuration exception", e);
+            }
         };
     }
 }
