774f8c2591f65fcccc4cda11be105f11f1df09e9
chenlei <1@1.com>
2019-09-06 19:03:02 +0800

    LoginAuthenticationFilter登陆过滤器修改

--- chen-f-spring-boot-starter/src/main/java/com/chen/f/spring/boot/configuration/springsecurity/filter/LoginAuthenticationFilter.java
+++ chen-f-spring-boot-starter/src/main/java/com/chen/f/spring/boot/configuration/springsecurity/filter/LoginAuthenticationFilter.java
@@ -1,13 +1,18 @@
 package com.chen.f.spring.boot.configuration.springsecurity.filter;
 
 import com.chen.f.spring.boot.configuration.springsecurity.token.LoginAuthenticationToken;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -17,25 +22,10 @@ import java.util.Map;
  * @since 2018/1/15 13:17.
  */
 public class LoginAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    public static final String CHEN_USERNAME_KEY = "username";
-    public static final String CHEN_PASSWORD_KEY = "password";
 
-    private String usernameParameter = CHEN_USERNAME_KEY;
-    private String passwordParameter = CHEN_PASSWORD_KEY;
     private boolean postOnly = true;
 
     public LoginAuthenticationFilter() {
-
-    }
-
-    @Override
-    public void setUsernameParameter(String usernameParameter) {
-        this.usernameParameter = usernameParameter;
-    }
-
-    @Override
-    public void setPasswordParameter(String passwordParameter) {
-        this.passwordParameter = passwordParameter;
     }
 
     @Override
@@ -53,26 +43,26 @@ public class LoginAuthenticationFilter extends UsernamePasswordAuthenticationFil
 
         String username;
         String password;
-        //if (StringUtils.startsWith(request.getContentType(), MediaType.APPLICATION_JSON_VALUE)) {
-        //    //适配json
-        //    ObjectMapper objectMapper = new ObjectMapper();
-        //    try {
-        //        Map<String, String> map = objectMapper.readValue(request.getInputStream(), new TypeReference<Map<String, String>>() {
-        //        });
-        //        map.forEach(request::setAttribute);
-        //        username = map.get(usernameParameter);
-        //        password = map.get(passwordParameter);
-        //    } catch (IOException e) {
-        //        logger.warn("获取登录信息出现异常", e);
-        //        throw new AuthenticationServiceException("获取登录信息出现异常", e);
-        //    }
-        //
-        //} else {
-            username = request.getParameter(usernameParameter);
-            password = request.getParameter(passwordParameter);
+        if (MediaType.APPLICATION_JSON_UTF8.isCompatibleWith(MediaType.valueOf(request.getContentType()))) {
+            //适配json
+            ObjectMapper objectMapper = new ObjectMapper();
+            try (ServletInputStream inputStream = request.getInputStream();) {
+                Map<String, String> map = objectMapper.readValue(inputStream, new TypeReference<Map<String, String>>() {
+                });
+                username = map.get(super.getUsernameParameter());
+                password = map.get(super.getPasswordParameter());
+                map.forEach(request::setAttribute);
+            } catch (IOException e) {
+                logger.warn("获取登录信息出现异常", e);
+                throw new AuthenticationServiceException("获取登录信息出现异常", e);
+            }
+
+        } else {
+            username = obtainUsername(request);
+            password = obtainPassword(request);
             Map<String, String[]> parameterMap = request.getParameterMap();
             parameterMap.forEach(request::setAttribute);
-        //}
+        }
 
 
         if (username == null) {
@@ -91,6 +81,7 @@ public class LoginAuthenticationFilter extends UsernamePasswordAuthenticationFil
 
         return super.getAuthenticationManager().authenticate(authRequest);
     }
+
 
 }
 
