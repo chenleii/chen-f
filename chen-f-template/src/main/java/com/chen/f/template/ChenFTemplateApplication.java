4348c204fd312d00d38ebd3373fe982563d51e1f
chenlei <1@1.com>
2019-11-07 16:39:14 +0800

    使用druid 不使用starter

--- chen-f-template/src/main/java/com/chen/f/template/ChenFTemplateApplication.java
+++ chen-f-template/src/main/java/com/chen/f/template/ChenFTemplateApplication.java
@@ -1,19 +1,15 @@
 package com.chen.f.template;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.chen.f.spring.boot.configuration.EnableChenFAdmin;
 import com.chen.f.spring.boot.configuration.EnableChenFCommon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
-
-@PropertySource("classpath:jdbc.properties")
 @EnableChenFCommon
 @EnableChenFAdmin
-@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class,})
+@SpringBootApplication
 public class ChenFTemplateApplication extends SpringBootServletInitializer {
 
     @Override
