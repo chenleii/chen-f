1a9b74939a49a936d47df50a68fec60c8e569a97
chenlei <1@1.com>
2020-03-04 13:59:54 +0800

    分布式锁+aop

--- chen-f-template/src/main/java/com/chen/f/template/ChenFTemplateApplication.java
+++ chen-f-template/src/main/java/com/chen/f/template/ChenFTemplateApplication.java
@@ -2,11 +2,13 @@ package com.chen.f.template;
 
 import com.chen.f.spring.boot.configuration.EnableChenFAdmin;
 import com.chen.f.spring.boot.configuration.EnableChenFCommon;
+import com.chen.f.spring.boot.configuration.distributedlock.EnableDistributedLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableDistributedLock
 @EnableChenFCommon
 @EnableChenFAdmin
 @SpringBootApplication
