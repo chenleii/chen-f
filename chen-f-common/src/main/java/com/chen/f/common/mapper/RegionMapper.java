123cccf9739088c0c327f5a3592e05855a347d91
chenlei <1@1.com>
2020-03-27 19:40:00 +0800

    查询上下级

--- chen-f-common/src/main/java/com/chen/f/common/mapper/RegionMapper.java
+++ chen-f-common/src/main/java/com/chen/f/common/mapper/RegionMapper.java
@@ -2,6 +2,10 @@ package com.chen.f.common.mapper;
 
 import com.chen.f.common.mybatisplus.SupperMapper;
 import com.chen.f.common.pojo.Region;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -14,4 +18,39 @@ import com.chen.f.common.pojo.Region;
  */
 public interface RegionMapper extends SupperMapper<Region> {
 
+    /**
+     * 根据ID查询当前记录和当前记录的所有下级记录
+     *
+     * @param id ID
+     * @return 当前记录和当前记录的所有下级记录
+     */
+    default List<Region> selectSubordinateById(@Param("id") String id) {
+        return selectSubordinateByIdList(Collections.singletonList(id));
+    }
+
+    /**
+     * 根据ID列表查询当前记录和当前记录的所有下级记录
+     *
+     * @param idList ID列表
+     * @return 当前记录和当前记录的所有下级记录
+     */
+    List<Region> selectSubordinateByIdList(@Param("idList") List<String> idList);
+
+    /**
+     * 根据ID查询当前记录和当前记录的所有上级记录
+     *
+     * @param id ID
+     * @return 当前记录和当前记录的所有上级记录
+     */
+    default List<Region> selectSuperiorById(@Param("id") String id){
+        return selectSuperiorByIdList(Collections.singletonList(id));
+    }
+
+    /**
+     * 根据ID列表查询当前记录和当前记录的所有上级记录
+     *
+     * @param idList ID列表
+     * @return 当前记录和当前记录的所有上级记录
+     */
+    List<Region> selectSuperiorByIdList(@Param("idList") List<String> idList);
 }
--- chen-f-common/src/main/java/com/chen/f/common/mapper/SysMenuMapper.java
+++ chen-f-common/src/main/java/com/chen/f/common/mapper/SysMenuMapper.java
@@ -3,6 +3,10 @@ package com.chen.f.common.mapper;
 
 import com.chen.f.common.mybatisplus.SupperMapper;
 import com.chen.f.common.pojo.SysMenu;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * <p>
@@ -13,5 +17,41 @@ import com.chen.f.common.pojo.SysMenu;
  * @since 2019-01-15
  */
 public interface SysMenuMapper extends SupperMapper<SysMenu> {
+
+    /**
+     * 根据ID查询当前记录和当前记录的所有下级记录
+     *
+     * @param id ID
+     * @return 当前记录和当前记录的所有下级记录
+     */
+    default List<SysMenu> selectSubordinateById(@Param("id") String id) {
+        return selectSubordinateByIdList(Collections.singletonList(id));
+    }
+
+    /**
+     * 根据ID列表查询当前记录和当前记录的所有下级记录
+     *
+     * @param idList ID列表
+     * @return 当前记录和当前记录的所有下级记录
+     */
+    List<SysMenu> selectSubordinateByIdList(@Param("idList") List<String> idList);
+
+    /**
+     * 根据ID查询当前记录和当前记录的所有上级记录
+     *
+     * @param id ID
+     * @return 当前记录和当前记录的所有下级记录
+     */
+    default List<SysMenu> selectSuperiorById(@Param("id") String id) {
+        return selectSuperiorByIdList(Collections.singletonList(id));
+    }
+
+    /**
+     * 根据ID列表查询当前记录和当前记录的所有上级记录
+     *
+     * @param idList ID列表
+     * @return 当前记录和当前记录的所有下级记录
+     */
+    List<SysMenu> selectSuperiorByIdList(@Param("idList") List<String> idList);
 
 }
