e3b63d9208532eaba74dca631eb45149eabb2081
chenlei <1@1.com>
2020-04-06 15:51:17 +0800

    没有找到api[{0}]

--- chen-f-common/src/main/java/com/chen/f/common/api/response/error/basic/BasicErrorResponses.java
+++ chen-f-common/src/main/java/com/chen/f/common/api/response/error/basic/BasicErrorResponses.java
@@ -70,7 +70,7 @@ public interface BasicErrorResponses extends ErrorResponse {
      * 没有找到API
      */
     static ErrorResponse notFoundApi(String api) {
-        String message = I18nHelper.getMessage(BasicErrorResponses.class.getName() + ".notFoundApi", new Object[]{api}, "没有找到api{0}");
+        String message = I18nHelper.getMessage(BasicErrorResponses.class.getName() + ".notFoundApi", new Object[]{api}, "没有找到api[{0}]");
         return ErrorResponse.create(message);
     }
 }
