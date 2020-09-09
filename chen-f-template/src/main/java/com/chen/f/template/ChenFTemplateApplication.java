9c75124b3c0d4f914edcf40b2175ca3ed8a38627
chen <wb-cl788111@alibaba-inc.com>
2020-09-09 19:57:04 +0800

    refactor: 修改为嵌入式数据库启动项目

--- chen-f-template/src/main/java/com/chen/f/template/ChenFTemplateApplication.java
+++ chen-f-template/src/main/java/com/chen/f/template/ChenFTemplateApplication.java
@@ -2,15 +2,19 @@ package com.chen.f.template;
 
 import com.chen.f.admin.configuration.EnableChenFAdmin;
 import com.chen.f.common.configuration.EnableChenFCommon;
+import com.chen.f.core.configuration.db.EnableChenFEmbeddedDataBase;
+import com.chen.f.core.configuration.redis.EnableChenFEmbeddedRedis;
+import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
+@EnableChenFEmbeddedDataBase
+@EnableChenFEmbeddedRedis
 @EnableChenFCommon
 @EnableChenFAdmin
-@SpringBootApplication
+@SpringBootApplication(exclude = {SpringBootConfiguration.class})
 public class ChenFTemplateApplication extends SpringBootServletInitializer {
 
     @Override
