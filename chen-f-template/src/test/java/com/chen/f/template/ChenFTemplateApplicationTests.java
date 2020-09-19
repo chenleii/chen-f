2f619c947eaf74c24f26290598614dea843d66ca
chenlei <1@1.com>
2020-09-20 02:52:21 +0800

    test: ChenFTemplateApplicationTests

--- chen-f-template/src/test/java/com/chen/f/template/ChenFTemplateApplicationTests.java
+++ chen-f-template/src/test/java/com/chen/f/template/ChenFTemplateApplicationTests.java
@@ -1,16 +1,16 @@
 package com.chen.f.template;
 
+import com.chen.f.core.configuration.redis.AutoConfigureTestRedis;
 import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.redis.AutoConfigureDataRedis;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 @AutoConfigureTestDatabase
-@AutoConfigureDataRedis
+@AutoConfigureTestRedis
 @ImportAutoConfiguration(exclude = {SpringBootConfiguration.class})
 public class ChenFTemplateApplicationTests {
 
