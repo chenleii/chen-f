6728b0f7f5ff75fbdeb27588190fabb2e69e9238
cl <wb-cl788111@alibaba-inc.com>
2021-08-24 14:35:07 +0800

    feat: ON DUPLICATE KEY UPDATE 实现改为非占位符方式,eg:column = values(column)。

--- chen-f-core/src/main/java/com/chen/f/core/mybatisplus/sqlinjector/InsertOnDuplicateKeyUpdate.java
+++ chen-f-core/src/main/java/com/chen/f/core/mybatisplus/sqlinjector/InsertOnDuplicateKeyUpdate.java
@@ -3,15 +3,20 @@ package com.chen.f.core.mybatisplus.sqlinjector;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.enums.SqlMethod;
 import com.baomidou.mybatisplus.core.injector.methods.Update;
+import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.sql.SqlScriptUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator;
 import org.apache.ibatis.executor.keygen.KeyGenerator;
 import org.apache.ibatis.executor.keygen.NoKeyGenerator;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author cl
@@ -68,8 +73,48 @@ public class InsertOnDuplicateKeyUpdate extends Update {
      * @return sql
      */
     protected String sqlOnDuplicateKeyUpdate(boolean logic, TableInfo table) {
-        String sqlScript = table.getAllSqlSet(logic, "");
-        sqlScript = SqlScriptUtils.convertTrim(sqlScript, "", "", ",", ",");
+        String sqlScript = getAllSqlSet(table, logic, "");
+        sqlScript = SqlScriptUtils.convertTrim(sqlScript, "", "", "", ",");
         return sqlScript;
     }
+
+
+    public String getAllSqlSet(TableInfo tableInfo, boolean ignoreLogicDelFiled, final String prefix) {
+        String newPrefix = prefix == null ? "" : prefix;
+        return tableInfo.getFieldList().stream().filter((i) -> {
+                    if (!ignoreLogicDelFiled) {
+                        return true;
+                    } else {
+                        return !tableInfo.isWithLogicDelete() || !i.isLogicDelete();
+                    }
+                }).map((i) -> getSqlSet(i, false, newPrefix))
+                .filter(Objects::nonNull).collect(Collectors.joining("\n", "\n", "\n"));
+    }
+
+    public String getSqlSet(TableFieldInfo tableFieldInfo, final boolean ignoreIf, final String prefix) {
+        String newPrefix = prefix == null ? "" : prefix;
+        String sqlSet = tableFieldInfo.getColumn() + "=";
+        if (StringUtils.isNotBlank(tableFieldInfo.getUpdate())) {
+            sqlSet = sqlSet + String.format(tableFieldInfo.getUpdate(), tableFieldInfo.getColumn());
+        } else {
+            sqlSet = sqlSet + "VALUES(" + tableFieldInfo.getColumn() + ")";
+        }
+
+        sqlSet = sqlSet + ",";
+        if (ignoreIf) {
+            return sqlSet;
+        } else {
+            if (tableFieldInfo.isWithUpdateFill()) {
+                return sqlSet;
+            } else {
+                try {
+                    String convertIfProperty = (String) MethodUtils.invokeMethod(tableFieldInfo, true, "convertIfProperty", newPrefix, tableFieldInfo.getProperty());
+                    return (String) MethodUtils.invokeMethod(tableFieldInfo, true, "convertIf", sqlSet, convertIfProperty, tableFieldInfo.getUpdateStrategy());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
 }
